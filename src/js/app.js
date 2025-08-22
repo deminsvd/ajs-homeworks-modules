f3a7f02cab93f0fbabc95c91a6f8233a63a08791
Sergey Demin <deminsv@gmail.com>
2025-08-22 12:32:35 +0300

    module-2 commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,6 @@
-console.log('app worked')
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from "./game.js"
+
+console.log('app worked');
+const game = new Game();
+game.start();
+console.log(Game, GameSavingData, loadGame, saveGame);
